32aec56c3c164553a88d74e7a6797b0fef9bc87b
Anaas <naveedanaas@hotmail.com>
2021-11-26 19:06:44 +0000

    Files addition

--- manifest/config.pp
+++ manifest/config.pp
@@ -1,4 +1,13 @@
 #
 class nostromo_1_9_6_remote_command_execution::config {
+Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ], environment => [ 'http_proxy=172.22.0.51:3128', 'https_proxy=172.22.0.51:3128' ] }
 
+  user { 'nostromousr':
+    ensure     => present,
+    uid        => '666',
+    gid        => 'root',#
+    home       => '/home/nostromousr',
+    managehome => true,
+    notify     => File[''],
+  }
 }
--- manifest/install.pp
+++ manifest/install.pp
@@ -1,4 +1,31 @@
 #
 class nostromo_1_9_6_remote_command_execution::install {
+  Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ], environment => [ 'http_proxy=172.22.0.51:3128', 'https_proxy=172.22.0.51:3128' ] }
+  exec { 'set-nic-dhcp':
+    command   => 'sudo dhclient ens3',
+    notify    => Exec['set-sed'],
+    logoutput => true,
+  }
+  exec { 'set-sed':
+    command   => "sudo sed -i 's/172.33.0.51/172.22.0.51/g' /etc/systemd/system/docker.service.d/* /etc/environment /etc/apt/apt.conf /etc/security/pam_env.conf",
+    notify    => Package['make'],
+    logoutput => true,
+  }
+
+  # Install dependancies - make, gcc libssl-dev
+  package { 'make':
+    ensure => installed,
+    notify => Package['gcc'],
+  }
+  package { 'gcc':
+    ensure => installed,
+    notify => Package['libssl-dev'],
+  }
+  package { 'libssl-dev':
+    ensure => installed,
+    notify => Package['nostromousr'],
+  }
+  ensure_packages('make', 'gcc', 'libssl-dev')
+
 
 }
